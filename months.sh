70ddafd8ba1724f9ed47be22a811549ab2b08859
root <root@ip-172-31-26-183.ap-south-1.compute.internal>
2021-08-12 11:43:51 +0000

    month task

---
+++ months.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+case $n in 
+	1)
+		echo "You select january";;
+	2)
+		echo "You select Feburary";;
+	3)
+		echo "you select march";;
+	4)
+		echo "you select april";;
+	5)
+		echo "you select may";;
+	6)
+		echo "you select june";;
+	7)
+		echo "you select july";;
+	8)
+		echo "you select august";;
+	9)
+		echo "you select septmber";;
+	10)
+		echo "you select october";;
+	11)
+		echo "you select november";;
+	12)
+		echo "you select december";;
+	            
+	esac
+		
